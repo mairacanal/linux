df0ee061bf79f063c296c6caa599c774149677f3
Maíra Canal <mairacanal@riseup.net>
2022-07-14 12:12:40 -0300

    drm/amd/display: Remove unused value0 variable
    Remove the variable value0 from the function
    dcn10_link_encoder_update_mst_stream_allocation_table.
    
    This was pointed by clang with the following warning:
    
    drivers/gpu/drm/amd/amdgpu/../display/dc/dcn10/dcn10_link_encoder.c:1223:11:
    warning: variable 'value0' set but not used [-Wunused-but-set-variable]
            uint32_t value0 = 0;
                     ^
    1 warning generated.
    
    Signed-off-by: Maíra Canal <mairacanal@riseup.net>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_link_encoder.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_link_encoder.c
@@ -1220,7 +1220,6 @@ void dcn10_link_encoder_update_mst_stream_allocation_table(
 	const struct link_mst_stream_allocation_table *table)
 {
 	struct dcn10_link_encoder *enc10 = TO_DCN10_LINK_ENC(enc);
-	uint32_t value0 = 0;
 	uint32_t value1 = 0;
 	uint32_t value2 = 0;
 	uint32_t slots = 0;
@@ -1321,8 +1320,6 @@ void dcn10_link_encoder_update_mst_stream_allocation_table(
 	 */
 	do {
 		udelay(10);
-
-		value0 = REG_READ(DP_MSE_SAT_UPDATE);
 
 		REG_GET(DP_MSE_SAT_UPDATE,
 				DP_MSE_SAT_UPDATE, &value1);

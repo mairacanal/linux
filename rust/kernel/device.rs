b8ce138ec9c87e4d7b72c1a0c121ef6da7925662
Maíra Canal <mcanal@igalia.com>
2023-02-10 14:07:39 -0300

    rust: device: add Device Resource Management abstraction
    Create an abstraction to allow Rust drivers to open a new devres group for
    the device with a defined id. Moreover, implement the related Drop trait
    to release the devres group.
    
    Signed-off-by: Maíra Canal <mcanal@igalia.com>

--- rust/kernel/device.rs
+++ rust/kernel/device.rs
@@ -9,6 +9,7 @@ use crate::{clk::Clk, error::from_kernel_err_ptr};
 
 use crate::{
     bindings,
+    prelude::ENOMEM,
     revocable::{Revocable, RevocableGuard},
     str::CStr,
     sync::{LockClassKey, NeedsLockClass, RevocableMutex, RevocableMutexGuard, UniqueArc},
@@ -212,6 +213,22 @@ impl Device {
         // requirements.
         unsafe { Self::new(dev.raw_device()) }
     }
+
+    /// Open a new devres group for dev with id.
+    pub fn open_group(&self, id: *mut core::ffi::c_void) -> Result<Resource> {
+        // SAFETY: The requirements are satisfied by the existence of `RawDevice` and its safety
+        // requirements.
+        let id = unsafe {
+            // Keep the allocation flags as GFP_KERNEL, as currently all users
+            // are using this flag.
+            bindings::devres_open_group(self.raw_device(), id, bindings::GFP_KERNEL)
+        };
+
+        if id.is_null() {
+            return Err(ENOMEM);
+        }
+        Ok(Resource { dev: self.raw_device(), id })
+    }
 }
 
 // SAFETY: The device returned by `raw_device` is the one for which we hold a reference.
@@ -228,6 +245,26 @@ impl Drop for Device {
         unsafe { bindings::put_device(self.ptr) };
     }
 }
+
+/// Device Resource Management
+pub struct Resource {
+    dev: *mut bindings::device,
+    id: *mut core::ffi::c_void,
+}
+
+impl Drop for Resource {
+    /// Release resources in a devres group
+    fn drop(&mut self) {
+        // SAFETY: The resource was properly allocated by the Device
+        unsafe { bindings::devres_release_group(self.dev, self.id) };
+    }
+}
+
+// SAFETY: The API doesn't allow access to the internal pointers.
+unsafe impl Send for Resource {}
+
+// SAFETY: The API doesn't allow access to the internal pointers.
+unsafe impl Sync for Resource {}
 
 /// Device data.
 ///

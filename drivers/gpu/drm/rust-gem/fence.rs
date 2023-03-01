21322bec46e072c702c271ccd48d3ec22e7fd528
Maíra Canal <mcanal@igalia.com>
2023-03-01 16:16:28 -0300

    drm/rust-gem: implement timeout to prevent hangs
    In order to prevent possible hangs, if the fence is not signaled for
    more than 10 seconds, force the fence to expire by being signaled.
    
    Signed-off-by: Maíra Canal <mcanal@igalia.com>

--- drivers/gpu/drm/rust-gem/fence.rs
+++ drivers/gpu/drm/rust-gem/fence.rs
@@ -1,18 +1,25 @@
 // SPDX-License-Identifier: MIT
 
 use core::fmt::Write;
+use core::ops::Deref;
+use core::time::Duration;
 use kernel::c_str;
 use kernel::dma_fence::*;
 use kernel::prelude::*;
+use kernel::timer::{Timer, TimerOps, UniqueTimer};
 
 #[derive(Default)]
-pub(crate) struct VgemFence {}
+pub(crate) struct VgemFenceOps {}
+
+pub(crate) struct VgemFence {
+    timer: Timer<VgemFenceOps, UniqueFence<VgemFenceOps>>,
+}
 
 static QUEUE_NAME: &CStr = c_str!("vgem_fence");
 static QUEUE_CLASS_KEY: kernel::sync::LockClassKey = kernel::sync::LockClassKey::new();
 
 #[vtable]
-impl FenceOps for VgemFence {
+impl FenceOps for VgemFenceOps {
     const USE_64BIT_SEQNO: bool = false;
 
     fn get_driver_name<'a>(self: &'a FenceObject<Self>) -> &'a CStr {
@@ -33,11 +40,32 @@ impl FenceOps for VgemFence {
     }
 }
 
-impl VgemFence {
-    pub(crate) fn create() -> Result<UniqueFence<Self>> {
-        let fence_ctx = FenceContexts::new(1, QUEUE_NAME, &QUEUE_CLASS_KEY)?;
+impl TimerOps for VgemFenceOps {
+    type Inner = UniqueFence<Self>;
 
-        pr_info!("vgem_fence_create: Creating fence...");
-        Ok(fence_ctx.new_fence(0, Default::default())?)
+    fn timer_callback(timer: &UniqueTimer<Self, UniqueFence<Self>>) {
+        let _ = timer.inner().signal();
+    }
+}
+
+impl VgemFence {
+    pub(crate) fn create() -> Result<VgemFence> {
+        let fence_ctx = FenceContexts::new(1, QUEUE_NAME, &QUEUE_CLASS_KEY)?;
+        let fence = fence_ctx.new_fence(0, Default::default())?;
+
+        let timer = Timer::<VgemFenceOps, UniqueFence<VgemFenceOps>>::setup(fence);
+
+        // We force the fence to expire within 10s to prevent driver hangs
+        timer.modify(Duration::from_secs(10));
+
+        Ok(VgemFence { timer })
+    }
+}
+
+impl Deref for VgemFence {
+    type Target = UniqueFence<VgemFenceOps>;
+
+    fn deref(&self) -> &Self::Target {
+        self.timer.inner()
     }
 }
